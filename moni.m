d34531318d292aa886477e10df903dfcb2421149
Administrator <3292645343@qq.com>
2021-11-27 17:15:03 +0800

    combine IS and CS

--- moni.m
+++ moni.m
@@ -10,11 +10,11 @@ s = fscanf(fids, '%f');
 cs = fscanf(fidCS, '%f');
 is = fscanf(fidIS, '%f');
 
-mn = 0.72 * ((10 * cs) .^ 2.37) + (1 - 0.72) * ((77.48 * is) .^ 0.92);
+mn = (1 - 0.3) * ((100 * cs) .^ 1.023) + 0.3 * ((100 * is) .^ 0.96);
 
 SROCC_CI = corr(s, mn, 'type', 'Spearman');
 KROCC_CI = corr(s, mn,'type','Kendall');
 PLCC_CI = corr(s, mn,'type','Pearson');
 RMSE_CI = sqrt(mean2((s - mn).^2));
 
-clearvars -except SROCC_CI KROCC_CI PLCC_CI RMSE_CI
+clearvars -except SROCC_CI KROCC_CI PLCC_CI RMSE_CI mn
